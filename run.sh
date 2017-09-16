61ff47163625f8b4f62979f292e2a1b3e480c6a7
Bruce <bruce@angel-tech.com.tw>
2017-09-17 00:44:28 +0800

    use docker pull

--- run.sh
+++ run.sh
@@ -2,6 +2,24 @@
 
 echo "done"
 pwd
+
+docker pull edgexfoundry/docker-edgex-volume
+docker pull edgexfoundry/docker-core-consul
+docker pull edgexfoundry/docker-core-config-seed
+docker pull edgexfoundry/docker-edgex-mongo
+docker pull edgexfoundry/docker-edgex-mongo-seed
+docker pull edgexfoundry/docker-support-logging
+docker pull edgexfoundry/docker-support-notifications
+docker pull edgexfoundry/docker-core-metadata
+docker pull edgexfoundry/docker-core-data
+docker pull edgexfoundry/docker-core-command
+docker pull edgexfoundry/docker-support-scheduler
+docker pull edgexfoundry/docker-export-client
+docker pull edgexfoundry/docker-export-distro
+docker pull edgexfoundry/docker-support-rulesengine
+docker pull edgexfoundry/docker-device-virtual
+
+
 sh deploy-edgeX.sh
 
 cd postman-test/bin
