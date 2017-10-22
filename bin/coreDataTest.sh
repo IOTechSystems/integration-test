6ee1084588e8d28a0dbfb6105fdcf5ff82675c51
Bruce <bruce@angel-tech.com.tw>
2017-10-22 12:26:18 +0800

    give UID

--- bin/coreDataTest.sh
+++ bin/coreDataTest.sh
@@ -22,24 +22,24 @@ echo "Info: Initiating Coredata Test."
 echo "[info] ---------- use docker run newman  ----------"
 echo "${PWD}/bin/postman-test"
 ls "${PWD}/bin/postman-test"
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="event" --iteration-data="data/eventData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="event_error_4xx" --iteration-data="data/eventData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
 
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="reading" --iteration-data="data/readingData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="reading_error_4xx" --iteration-data="data/readingData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
 
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="valuedescriptor" --iteration-data="data/valueDescriptorData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
-docker-compose run --rm -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
+docker-compose run --rm --user="${UID}" -v "${PWD}/bin/postman-test":/etc/newman postman run ${COLLECTION_PATH} \
     --folder="valuedescriptor_error_4xx" --iteration-data="data/valueDescriptorData.json" --environment=${ENV_PATH} \
     --reporters="junit,cli"
 
