16afaaaf9b37cf1c1f3fa5d126d69ddb384207dd
Bruce <bruce@angel-tech.com.tw>
2017-10-22 14:26:52 +0800

    refactor script

--- bin/importCoreDataDump.sh
+++ bin/importCoreDataDump.sh
@@ -21,7 +21,7 @@ do
 
     if [ -f ${DUMP_FILES[index]} ]; then
         COPY_FROM="${DUMP_FILES[index]}"
-        COPY_TO=${COPY_FROM#t*postman}
+        COPY_TO=${COPY_FROM%bpostman}
 
         echo "${COPY_FROM} ~ $COPY_TO"
 
