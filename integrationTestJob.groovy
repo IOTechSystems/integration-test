0dce4f4966fb389fab878e84303640a6f3d855a6
Bruce <bruce@angel-tech.com.tw>
2017-10-28 02:01:34 +0800

    test core data

--- integrationTestJob.groovy
+++ integrationTestJob.groovy
@@ -1,35 +1,35 @@
-def runNode(nodeName) {
-        node(nodeName) {
-            checkout scm
+def runNode() {
 
-            try {
+    checkout scm
 
-                stage('Startup test services') {
-                    sh 'sh deploy-edgeX.sh'
-                    sh 'docker-compose exec -T volume ls /etc/newman'
-                }
+    try {
 
-
-                stage('Run Postman test') {
-
-                    sh './bin/run.sh -cd'
-
-                    junit 'bin/postman-test/newman/**.xml'
-
-                    sh 'docker-compose exec -T volume rm -rf /etc/newman/newman/'
-                }
-
-
-            }catch (e) {
-                echo 'Something failed!'
-                throw e;
-            }finally{
-                stage('Shutdown test services') {
-                    echo '[INFO] test end !'
-                    sh 'docker-compose down'
-                }
-            }
+        stage('Startup test services') {
+            sh 'sh deploy-edgeX.sh'
+            sh 'docker-compose exec -T volume ls /etc/newman'
         }
+
+
+        stage('Run Postman test') {
+
+            sh './bin/run.sh -cd'
+
+            junit 'bin/postman-test/newman/**.xml'
+
+            sh 'docker-compose exec -T volume rm -rf /etc/newman/newman/'
+        }
+
+
+    }catch (e) {
+        echo 'Something failed!'
+        throw e;
+    }finally{
+        stage('Shutdown test services') {
+            echo '[INFO] test end !'
+            sh 'docker-compose down'
+        }
+    }
+
 }
 
 return this
