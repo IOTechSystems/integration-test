346a4a84afb13a6d3dfd6e3dfa0a515f4a610558
Bruce <bruce@angel-tech.com.tw>
2017-10-28 11:05:59 +0800

    test volume

--- test.sh
+++ test.sh
@@ -2,5 +2,7 @@
 
 VOLUME_CONTAINER=$(docker-compose ps -q volume)
 VOLUME_CONTAINER=`echo ${VOLUME_CONTAINER} | cut -b 1-12`
+
+echo ${VOLUME_CONTAINER}
 docker cp ${PWD}/bin/postman-test "${VOLUME_CONTAINER}":/etc/newman
 docker-compose exec -T  volume ls /etc/newman
