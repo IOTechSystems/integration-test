b0885218648a15ca8cb418ee312ef95873a83904
Bruce <bruce@angel-tech.com.tw>
2017-10-28 15:39:40 +0800

    test volume

--- bin/flushCoreDataDump.sh
+++ bin/flushCoreDataDump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NAMESFILE=$(dirname "$0")/files.sh
-EVENTDUMPJS=$(dirname "$0")/javascript/coredata/event.js
-READINGDUMPJS=$(dirname "$0")/javascript/coredata/reading.js
-VDDUMPJS=$(dirname "$0")/javascript/coredata/valueDescriptor.js
+EVENTDUMPJS=/etc/newman/javascript/coredata/event.js
+READINGDUMPJS=/etc/newman/javascript/coredata/reading.js
+VDDUMPJS=/etc/newman/javascript/coredata/valueDescriptor.js
 
 
 if [ -f $NAMESFILE ]; then 
@@ -22,14 +22,14 @@ FLUSH_SCRIPTS=( $EVENTDUMPJS $READINGDUMPJS $VDDUMPJS)
 for index in "${!FLUSH_SCRIPTS[@]}"
 do
     if [ -f ${FLUSH_SCRIPTS[index]} ]; then
-        COPY_FROM="${FLUSH_SCRIPTS[index]}"
-        COPY_TO="${RANDOM}.json"
-
-        MONGO_CONTAINER=$(docker-compose ps -q mongo)
-        MONGO_CONTAINER=`echo ${MONGO_CONTAINER} | cut -b 1-12`
-
-        docker cp ${COPY_FROM} "${MONGO_CONTAINER}":${COPY_TO}
-        docker-compose exec -T mongo /bin/bash -c "mongo ${DATA_BASE} ${COPY_TO}"
+#        COPY_FROM="${FLUSH_SCRIPTS[index]}"
+#        COPY_TO="${RANDOM}.json"
+#
+#        MONGO_CONTAINER=$(docker-compose ps -q mongo)
+#        MONGO_CONTAINER=`echo ${MONGO_CONTAINER} | cut -b 1-12`
+#
+#        docker cp ${COPY_FROM} "${MONGO_CONTAINER}":${COPY_TO}
+        docker-compose exec -T mongo /bin/bash -c "mongo ${DATA_BASE} ${FLUSH_SCRIPTS[index]}"
 
         echo "Info: ${FLUSH_SCRIPTS[index]} data flushed"
 
