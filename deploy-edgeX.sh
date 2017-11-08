058e898c0bd009a1d406c56e1b12a412b1bc21e1
Bruce <bruce@angel-tech.com.tw>
2017-11-08 19:49:19 +0800

    docker-compose image name from env

--- deploy-edgeX.sh
+++ deploy-edgeX.sh
@@ -35,7 +35,6 @@ declare -A IMAGES=(
     ["deviceVirtual"]="edgexfoundry/docker-device-virtual"
 )
 
-echo "params.TEST_SERVICE_IMAGE ${params.TEST_SERVICE_IMAGE}"
 echo "${TEST_SERVICE}"
 if [ -z "${TEST_SERVICE}" ]; then
     echo "TEST_SERVICE not set , use default"
