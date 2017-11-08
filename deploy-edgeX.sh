1066adc02ec4f36e94f2d2e6fc3901922d1c867c
Bruce <bruce@angel-tech.com.tw>
2017-11-08 19:28:05 +0800

    docker-compose image name from env

--- deploy-edgeX.sh
+++ deploy-edgeX.sh
@@ -22,13 +22,25 @@ declare -A IMAGES=(
     ["volume"]="edgexfoundry/docker-edgex-volume"
     ["consul"]="edgexfoundry/docker-core-consul"
     ["coreMetadata"]="edgexfoundry/docker-core-metadata"
+    ["mongo"]="edgexfoundry/docker-edgex-mongo"
+    ["mongoSeed"]="edgexfoundry/docker-edgex-mongo-seed"
+    ["supportLogging"]="edgexfoundry/docker-support-logging"
+    ["coreMetadata"]="edgexfoundry/docker-core-metadata"
+    ["coreData"]="edgexfoundry/docker-core-data"
+    ["coreCommand"]="edgexfoundry/docker-core-command"
+    ["supportScheduler"]="edgexfoundry/docker-support-scheduler"
+    ["exportClient"]="edgexfoundry/docker-export-client"
+    ["exportDistro"]="edgexfoundry/docker-export-distro"
+    ["supportRulesengine"]="edgexfoundry/docker-support-rulesengine"
+    ["deviceVirtual"]="edgexfoundry/docker-device-virtual"
 )
 
 echo "${TEST_SERVICE}"
 if [ -z "${TEST_SERVICE}" ]; then
     echo "TEST_SERVICE not set , use default"
 else
-     IMAGES["${TEST_SERVICE}"]="123123"
+    echo "TEST_SERVICE has set , replace default"
+    IMAGES["${TEST_SERVICE}"]="${TEST_SERVICE_IMAGE}"
 fi
 
 
