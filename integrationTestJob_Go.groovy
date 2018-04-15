fece629e4cdf3160d320bf4cb8e7a3da69124e1b
Bruce <bruce@angel-tech.com.tw>
2018-04-15 19:08:43 +0800

    add go version integration test

---
+++ integrationTestJob_Go.groovy
@@ -0,0 +1,70 @@
+
+def runNode() {
+    def envMap =[
+        'volume':'edgexfoundry/docker-edgex-volume',
+        'consul':'edgexfoundry/docker-core-consul',
+        'coreMetadata':'edgexfoundry/docker-core-metadata',
+        'mongo':'edgexfoundry/docker-edgex-mongo',
+        'mongoSeed':'edgexfoundry/docker-edgex-mongo-seed',
+        'supportLogging':'edgexfoundry/docker-support-logging',
+        'coreMetadata':'docker.iotechsys.com/edgexpert/core-metadata-go-x86_64:0.5.0',
+        'coreData':'docker.iotechsys.com/edgexpert/core-data-go-x86_64:0.5.0',
+        'coreCommand':'docker.iotechsys.com/edgexpert/core-command-go-x86_64:0.5.0',
+        'supportScheduler':'edgexfoundry/docker-support-scheduler',
+        'exportClient':'edgexfoundry/docker-export-client',
+        'exportDistro':'edgexfoundry/docker-export-distro',
+        'supportRulesengine':'edgexfoundry/docker-support-rulesengine',
+        'deviceVirtual':'edgexfoundry/docker-device-virtual'
+    ]
+    if(params.TEST_SERVICE==null){
+        print "test_service is null"
+    }else{
+        print "test_service is exist ,set to envMap"
+        envMap.put(params.TEST_SERVICE,params.TEST_SERVICE_IMAGE)
+    }
+
+    def envList = []
+
+    for ( e in envMap ) {
+        print "set env ${e.key}=${e.value}"
+        envList.add("${e.key}=${e.value}")
+    }
+
+    withEnv(envList) {
+        try {
+
+            stage('Startup test services') {
+                sh 'bash ./deploy-edgeX.sh'
+            }
+
+            stage('Run Postman test') {
+                sh 'rm -rf bin/testResult'
+
+                sh './bin/run.sh -all'
+
+                junit 'bin/testResult/**.xml'
+
+            }
+
+
+        }catch (e) {
+            echo 'Something failed!'
+            throw e;
+        }finally{
+            stage('Shutdown test services') {
+                echo '[INFO] test end !'
+                sh 'docker-compose down -v'
+                try{
+                    sh 'docker rmi $(docker images -f dangling=true -q)'
+                }catch (e){
+                    echo 'clear done , none dangling image !'
+                }
+
+            }
+        }
+    }
+
+
+}
+
+return this
