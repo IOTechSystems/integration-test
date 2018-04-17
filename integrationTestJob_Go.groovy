9187ef4ab99b114be997c7c4dc65ef63d4040adc
Bruce <bruce@angel-tech.com.tw>
2018-04-17 22:29:37 +0800

    update docker-compose file from edgex

--- integrationTestJob_Go.groovy
+++ integrationTestJob_Go.groovy
@@ -52,6 +52,7 @@ def runNode() {
             stage('Shutdown test services') {
                 echo '[INFO] test end !'
                 sh 'docker-compose down -v'
+                sh 'docker volume prune -f'
                 try{
                     sh 'docker rmi $(docker images -f dangling=true -q)'
                 }catch (e){
