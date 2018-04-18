1eb534562c5ec4d37e43e7cef536c825f65d6f35
Bruce <bruce@angel-tech.com.tw>
2018-04-18 22:17:40 +0800

    mongo should volume newman folder which has dump data

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-VOLUME_CONTAINER=$(docker-compose ps -q volume)
-VOLUME_CONTAINER=`echo ${VOLUME_CONTAINER} | cut -b 1-12`
+#VOLUME_CONTAINER=$(docker-compose ps -q volume)
+#VOLUME_CONTAINER=`echo ${VOLUME_CONTAINER} | cut -b 1-12`
+#
+#echo "VOLUME_CONTAINER ${VOLUME_CONTAINER}"
+#docker cp $(dirname "$0")/bin/postman-test/. "${VOLUME_CONTAINER}":/etc/newman
+#docker-compose exec -T  volume ls /etc/newman/
 
-echo "VOLUME_CONTAINER ${VOLUME_CONTAINER}"
-docker cp $(dirname "$0")/bin/postman-test/. "${VOLUME_CONTAINER}":/etc/newman
-docker-compose exec -T  volume ls /etc/newman/
+. $(dirname "$0")/test_env.sh
+
+#sh deploy-edgex.sh
+
+sh ./bin/run.sh -cd
---
+++ test_env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export volume=edgexfoundry/docker-edgex-volume
+export coreMetadata=edgexfoundry/docker-core-metadata
+export mongo=edgexfoundry/docker-edgex-mongo
+export supportLogging=edgexfoundry/docker-support-logging
+export coreMetadata=edgexfoundry/docker-core-metadata
+export coreData=edgexfoundry/docker-core-data
+export coreCommand=edgexfoundry/docker-core-command
+export supportScheduler=edgexfoundry/docker-support-scheduler
+export exportClient=edgexfoundry/docker-export-client
+export exportDistro=edgexfoundry/docker-export-distro
+export supportRulesengine=edgexfoundry/docker-support-rulesengine
+export deviceVirtual=edgexfoundry/docker-device-virtual
