576b9c63989983f6f0e04aa9f89b723e2cec2b2f
Bruce <bruce@angel-tech.com.tw>
2018-04-20 08:52:06 +0800

    modify echo script

--- deploy-edgeX.sh
+++ deploy-edgeX.sh
@@ -43,7 +43,7 @@ run_service mongo
 #sleep 5
 #run_service mongo-seed
 
-docker-compose exec -T config-seed nc
+docker ps -a
 
 while ! $(docker-compose exec -T config-seed nc -z localhost 8500);do echo "not already startup… wait for 5 second reconnect." ;sleep 5; done
 
