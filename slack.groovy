7ccc1d8ca7feac355fe2522395eda5d5b23342b3
Bruce <bruce@angel-tech.com.tw>
2018-04-26 23:18:47 +0800

    try use slack integration
    Signed-off-by: Bruce <bruce@angel-tech.com.tw>

--- slack.groovy
+++ slack.groovy
@@ -8,8 +8,6 @@ def post(color, message) {
     def detail_link   = "(<${env.BUILD_URL}|Open>)"
     def slack_msg     = "job ${env.JOB_NAME}[No.${env.BUILD_NUMBER}] was test ${currentBuild.result}. ${detail_link} \n\n ${message}"
 
-    withCredentials([string(credentialsId: '6xw3fGZbX8BWiY87xprk8DM3', variable: 'Token')]) {
-        slackSend channel: "${slack_channel}", color: "${color}", message: "${slack_msg}", teamDomain: "${slack_domain}", token: "${Token}"
-    }
+    slackSend channel: "${slack_channel}", color: "${color}", message: "${slack_msg}", teamDomain: "${slack_domain}", token: "6xw3fGZbX8BWiY87xprk8DM3"
 }
 return this
