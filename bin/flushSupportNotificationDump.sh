e65f8c6d8ff914c22b07bad3bde0341777297dfd
Bruce <weichou1229@gmail.com>
2018-05-26 20:21:54 +0800

    refactor flush script
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/flushSupportNotificationDump.sh
+++ bin/flushSupportNotificationDump.sh
@@ -2,15 +2,8 @@
 
 DATA_BASE="notifications"
 
-SUBSCRIPTION_JS=/etc/newman/javascript/supportNotifications/flushScript.js
+FLUSH_SCRIPT=/etc/newman/javascript/supportNotifications/flushScript.js
 
-FLUSH_SCRIPTS=( $SUBSCRIPTION_JS )
+docker-compose exec -T mongo /bin/bash -c "mongo ${DATA_BASE} ${FLUSH_SCRIPT}"
 
-for index in "${!FLUSH_SCRIPTS[@]}"
-do
-    docker-compose exec -T mongo /bin/bash -c "mongo ${DATA_BASE} ${FLUSH_SCRIPTS[index]}"
-
-    echo "Info: ${FLUSH_SCRIPTS[index]} data flushed"
-
-done
-
+echo "Info: ${FLUSH_SCRIPT} data flushed"
