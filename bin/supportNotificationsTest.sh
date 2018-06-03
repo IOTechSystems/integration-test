687372973cbfe1fbd636efc075797e162ad389a9
Bruce <weichou1229@gmail.com>
2018-06-03 19:43:32 +0800

    without use iteration-data in support-notification test
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/supportNotificationsTest.sh
+++ bin/supportNotificationsTest.sh
@@ -8,5 +8,5 @@ echo "Info: Initiating Support Notifications Test."
 echo "[info] ---------- use docker-compose run newman ----------"
 
 docker-compose run --rm postman run ${COLLECTION_PATH} \
-    --folder="subscription" --iteration-data="data/supportNotifications.json" --environment=${ENV_PATH} \
+    --folder="subscription"  --environment=${ENV_PATH} \
     --reporters="junit,cli"
