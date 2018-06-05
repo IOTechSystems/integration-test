9822c17c43bd7e90eccb3f1e4e2ab1fed25fc328
Bruce <weichou1229@gmail.com>
2018-06-05 21:26:59 +0800

    can test notification , transmission api by newman
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- bin/supportNotificationsTest.sh
+++ bin/supportNotificationsTest.sh
@@ -7,6 +7,6 @@ echo "Info: Initiating Support Notifications Test."
 
 echo "[info] ---------- use docker-compose run newman ----------"
 
-docker-compose run --rm postman run ${COLLECTION_PATH} \
-    --folder="subscription"  --environment=${ENV_PATH} \
-    --reporters="junit,cli"
+docker-compose run --rm postman run ${COLLECTION_PATH} --folder="subscription"  --environment=${ENV_PATH} --reporters="junit,cli"
+docker-compose run --rm postman run ${COLLECTION_PATH} --folder="notification"  --environment=${ENV_PATH} --reporters="junit,cli"
+docker-compose run --rm postman run ${COLLECTION_PATH} --folder="transmission"  --environment=${ENV_PATH} --reporters="junit,cli"
