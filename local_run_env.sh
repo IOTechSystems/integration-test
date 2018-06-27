d1f148395368577060d12d5260388645e73e06b1
Bruce <weichou1229@gmail.com>
2018-06-27 20:00:06 +0800

    migrate go version to 1.0.0
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- local_run_env.sh
+++ local_run_env.sh
@@ -27,4 +27,4 @@ export exportDistro=docker.iotechsys.com/edgexpert/export-distro-go-x86_64:1.0.0
 export supportScheduler=docker.iotechsys.com/edgexpert/support-scheduler-java-x86_64:1.0.0
 
 export supportRulesengine=edgexfoundry/docker-support-rulesengine:0.2.1
-export deviceVirtual=edgexfoundry/docker-device-virtual:0.2.1
+export deviceVirtual=docker.iotechsys.com/edgexpert/device-virtual-java-x86_64:1.0.0
