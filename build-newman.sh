c232503985972707922dfc326355bd30e57111f6
Tom Fleming <tom@iotechsys.com>
2018-08-14 11:48:38 +0100

    E10R-25 updated newman build
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- build-newman.sh
+++ build-newman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 . ./arch.sh
-cd newman/docker/images/alpine33
+cd newman/docker/images/ubuntu1404
 docker build --build-arg NEWMAN_VERSION=4.0.2 -t docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2 .
 docker push docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2
