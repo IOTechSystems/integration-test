87758bf892df092b859bce0c68f0d190059c1f8e
Tom Fleming <tom@iotechsys.com>
2018-08-14 15:55:02 +0100

    E10R-25 changed newman image names
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- build-newman.sh
+++ build-newman.sh
@@ -2,5 +2,5 @@
 
 . ./arch.sh
 cd newman/docker/images/alpine37
-docker build --build-arg NEWMAN_VERSION=4.0.2 -t docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2 .
-docker push docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2
+docker build --build-arg NEWMAN_VERSION=4.0.2 -t docker.iotechsys.com/services/newman_$EX_ARCH:4.0.2 .
+docker push docker.iotechsys.com/services/newman_$EX_ARCH:4.0.2
