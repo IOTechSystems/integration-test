0ce191050916eb66c3d37f5ab03852d6b21c1cfb
Tom Fleming <tom@iotechsys.com>
2018-08-14 11:01:48 +0100

    E10R-25 minor fix
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- build-newman.sh
+++ build-newman.sh
@@ -2,5 +2,5 @@
 
 . ./arch.sh
 cd newman/docker/images/alpine33
-docker build --build-arg NEWMAN_VERSION=4.0.2 -t iotech_newman_$EX_ARCH:4.0.2 .
+docker build --build-arg NEWMAN_VERSION=4.0.2 -t docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2 .
 docker push docker.iotechsys.com/services/iotech_newman_$EX_ARCH:4.0.2
