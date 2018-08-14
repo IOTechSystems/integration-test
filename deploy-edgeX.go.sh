f986b14eadd8f78cd2c8bd92bdf853b2927c9649
Tom Fleming <tom@iotechsys.com>
2018-08-14 09:58:15 +0100

    E10R-25 updated EX_ARCH specification
    Signed-off-by: Tom Fleming <tom@iotechsys.com>

--- deploy-edgeX.go.sh
+++ deploy-edgeX.go.sh
@@ -25,7 +25,21 @@
 #    . $(dirname "$0")/bin/env.sh
 #fi
 export EX_ARCH=`uname -m`
-
+case $EX_ARCH in
+  armv7l)
+    EX_ARCH=arm
+  ;;
+  aarch64)
+    EX_ARCH=arm64
+  ;;
+  x86_64)
+    EX_ARCH=x86_64
+  ;;
+  *)
+    echo "Unsupported: $EX_ARCH"
+    exit 2
+  ;;
+esac
 
 run_service () {
 	echo "[INFO] Starting.. $1"
